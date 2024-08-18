7e20b4b0ca80e986d61516895366743b40d700a9
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-08-18 21:39:14 +0300

    js operators

--- 01-base/app.js
+++ 01-base/app.js
@@ -1,29 +1,28 @@
-let number = 10;
-let firstName = "Muhammadjon";
+// let number = 10;
+// let firstName = "Muhammadjon";
 
-const isProgrammer = true;
+// const isProgrammer = true;
 
-number = 7;
-firstName = "Oybek";
+// number = 7;
+// firstName = "Oybek";
 
 // alert(firstName);
-console.log("My name is:", firstName);
+// console.log("My name is:", firstName);
 
-//! ⁡⁢⁣⁢isProgrammer = false; = error because of conts
+//! ⁡⁢⁣⁢isProgrammer = false; = Error because of conts
 
+// * ⁡Can do⁡
 
-//* ⁡⁢⁢⁡⁣⁢⁣Can do⁡
-
-let something = "Avaible variable name";
-let $ = true;
-let $number = 100;
+let something = "Avaible variable name"; //& String
+let $ = true; //& Boolean
+let $number = 100; //& Number
 let number$ = 99;
 let _ = false;
 let _city = "New York";
 let country_ = "United States";
-let my_car = "Mercedes cls63"; //^ ⁡⁢⁣⁣Not good way to name a variable with pascal_cas⁡⁢⁣⁣e⁡
-let myLaptop = "HP 250 G8"; //* ⁡⁣⁢⁣This is way better than pascal_case this is camelCase that the most used in js⁡
 let myApartment27 = true;
+let my_car = "Mercedes cls63"; //^ ⁡⁢⁣⁣Not good way to name a variable with pascal_cas⁡⁢⁣⁣e⁡
+let myLaptop = "HP 250 G8"; //* ⁣This is way better than pascal_case this is camelCase that the most used in js⁡
 
 //! Restricted
 
@@ -32,3 +31,32 @@ let myApartment27 = true;
 //! let let = 'let'
 //! let false = false;
 //! let const = 'const';
+
+//* OPERATORS IN JS
+
+// let number = 100;
+
+// console.log(number + 70);
+// console.log(number - 70);
+// console.log(number * 70);
+// console.log(number / 70);
+// console.log(number); //? Will this operations in the top will change the actual value of the variable ? Answer: NO
+
+// let number2 = number + 10; //& Answer: 110
+// console.log(number, number2); //& Answer: 100 110 = number = 100, number2 = 110
+
+// number = number2 - number; //& Answer: 10
+// number2 = number2 + 1; //& Answer: 111
+// console.log(number, number2); //& Answer: 10 111
+
+// let num = 50;
+
+// let num2 = num + (10 * 2) / 5 - 1; //& Answer: how in math and same in js: 50 + (10 * 2) / 5 - 1 = 53
+// console.log(num2); //& Answer: 53
+
+const firstName = "Muhammadjon";
+const secondName = "Olimov";
+
+const fullName = firstName + " " + secondName; //& Answer: Muhammadjon + Olimov = Muhammadjon Olimov | " " = the space
+
+console.log(fullName); //& Answer: Muhammadjon Olimov
