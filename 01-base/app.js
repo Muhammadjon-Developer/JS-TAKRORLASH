9f10948c454775133b1fefeca5a168a21d9efb7d
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-08-18 19:59:27 +0300

    js base

---
+++ 01-base/app.js
@@ -0,0 +1,34 @@
+let number = 10;
+let firstName = "Muhammadjon";
+
+const isProgrammer = true;
+
+number = 7;
+firstName = "Oybek";
+
+// alert(firstName);
+console.log("My name is:", firstName);
+
+//! ⁡⁢⁣⁢isProgrammer = false; = error because of conts
+
+
+//* ⁡⁢⁢⁡⁣⁢⁣Can do⁡
+
+let something = "Avaible variable name";
+let $ = true;
+let $number = 100;
+let number$ = 99;
+let _ = false;
+let _city = "New York";
+let country_ = "United States";
+let my_car = "Mercedes cls63"; //^ ⁡⁢⁣⁣Not good way to name a variable with pascal_cas⁡⁢⁣⁣e⁡
+let myLaptop = "HP 250 G8"; //* ⁡⁣⁢⁣This is way better than pascal_case this is camelCase that the most used in js⁡
+let myApartment27 = true;
+
+//! Restricted
+
+//! let 6cars = 'My Cars';
+//! let food-price = "10$";
+//! let let = 'let'
+//! let false = false;
+//! let const = 'const';
