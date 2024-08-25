66743cbfb52b4dfe76be4f2be58bcf6e05515992
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-08-25 21:19:20 +0300

    part 2 last part string methods

--- 03-theory/02-strings.js
+++ 03-theory/02-strings.js
@@ -1,12 +1,25 @@
-const name = "Muhammadjon";
+const firstName = "Muhammadjon";
 const age = 14;
 
 function getAge() {
   return age;
 }
 
-// const old = 'Hello my name is ' + name + ' and my age is ' + age;
+// const old = 'Hello my name is ' + firstName + ' and my age is ' + age;
 
-const output = `Hello my name is ${name} and my age is ${getAge()}`;
+// const output = `Hello my name is ${firstName} and my age is ${
+//   getAge() > 18 ? "Can drive" : "Can't drive"
+// }`;
 
-console.log(output);
+// console.log(output);
+
+// console.log(firstName.length); // length - String uzunligi
+// console.log(firstName.toUpperCase()); // toUpperCase() - Stringdagi barcha harflarni katta qilish uchun
+// console.log(firstName.toLowerCase()); // toLowerCase() - Stringdagi barcha harflarni kichik qilish uchun
+// console.log(firstName.charAt(2)); // charAt(index) - Qaysi index da qaysi elemnt borligini ko'rsatadi
+// console.log(firstName.indexOf("a")); // indexOd(element) - Lyuboy elementni qaysi index da ekanligini ko'rsatadi
+// console.log(firstName.startsWith("Muham")); // startsWith() - Stringimiz shu so'zlardan boshlanishi yoki yo'q ligini aniqlash uchun agar shunday so'zlardan bo'lsa true, aks holda false qaytaradi
+// console.log(lastName.endsWith("jon")); // // endsWith() - Stringimiz shu so'zlardan tugashini yoki yo'q ligini aniqlash uchun agar shunday so'zlardan bo'lsa true, aks holda false qaytaradi
+// console.log(firstName.repeat(9999)); // repeat(necha marta) - Stringimiz necha marta dublirovka bo'lishini aniqlash
+// const password = "   MySuperPassword    ";
+// console.log(password.trim().length); // trim() - O'ng va chapdan probellarni olib tashlash 
